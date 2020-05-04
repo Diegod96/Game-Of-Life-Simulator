7dab98abd88cd6ee1577793a6bfca1d041f5d23d
root <ddphillyfan@gmail.com>
2020-05-03 20:14:44 -0400

    Implemented Simulation class

--- src/main/java/com/diego/App.java
+++ src/main/java/com/diego/App.java
@@ -13,8 +13,6 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-
-
         Label label = new Label("Hello, JavaFX ");
         Scene scene = new Scene(new StackPane(label), 640, 480);
         stage.setScene(scene);
