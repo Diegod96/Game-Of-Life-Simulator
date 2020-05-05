d32ea317b7ef9bc10c5692e448c780d68e670481
Diegod96 <ddphillyfan@gmail.com>
2020-05-04 22:00:11 -0400

    Implemented start and stop button/functionality

--- src/main/java/com/diego/Toolbar.java
+++ src/main/java/com/diego/Toolbar.java
@@ -10,21 +10,31 @@ public class Toolbar extends ToolBar {
 
     public Toolbar(MainView mainView) {
         this.mainView = mainView;
-
         Button draw = new Button("Draw");
         draw.setOnAction(this::handleDraw);
-
         Button erase = new Button("Erase");
         erase.setOnAction(this::handleErase);
-
         Button step = new Button("Step");
         step.setOnAction(this::handleStep);
-
         Button reset = new Button("Reset");
         reset.setOnAction(this::handleReset);
-       
-        this.getItems().addAll(draw, erase, reset, step);
+        Button start = new Button("Start");
+        start.setOnAction(this::handleStart);
+        Button stop = new Button("Stop");
+        stop.setOnAction(this::handleStop);
 
+        this.getItems().addAll(draw, erase, reset, step, start, stop);
+    }
+
+
+
+    private void handleStop(ActionEvent actionEvent) {
+        this.mainView.getSimulator().stop();
+    }
+
+    private void handleStart(ActionEvent actionEvent) {
+        this.mainView.setApplicationState(MainView.SIMULATING);
+        this.mainView.getSimulator().start();
     }
 
     private void handleReset(ActionEvent actionEvent) {
